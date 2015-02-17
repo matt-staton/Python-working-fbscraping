6a134f3400976144ddb12fff148704ffc5cecca9
Vicky Qijing Zhang <vickyqijingzhang@gmail.com>
2015-02-16 22:31:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 __author__ = 'Vicky'
 
 # reference: http://simplebeautifuldata.com/2014/08/25/simple-python-facebook-scraper-part-1/
-import lib2to3
-import urllib2
+
+import urllib.request
 import json
 import pandas
-
-
 
 
 def main():
@@ -22,11 +20,13 @@ def main():
             current_page = graph_url + company
 
             # open public page in facebook api  (the most important part really. should memorize it -- open, read, loads)
-            web_response = urllib2.urlopen(current_page) # taking our URL string we created and storing the response
-            readable_page = web_response.read() #  converts our response object into a readable 'html' like document that you can print to console
+            web_response = urllib.request.urlopen(current_page) # taking our URL string we created and storing the response
+            #readable_page = web_response.read() #  converts our response object into a readable 'html' like document that you can print to console
+            readable_page = web_response.readall().decode('utf-8') # work-around for python 3
             json_fbpage = json.loads(readable_page) # taking our readable_page variable and converting it to a JSON object
             # This will allow us to refer to information within that original mess, by key value pairs, kind of like a Python dictionary.
-            # json_fbpage is already a dict object at this time. If something goes wrong, check all object types again.
+            # json_fbpage is already a dict ob
+            # ject at this time. If something goes wrong, check all object types again.
             list_json.append(json_fbpage) # if multiple json objects, you need to put them into a list first, and dump the whole list
             #print(json_fbpage)
 
@@ -41,15 +41,20 @@ def main():
 
             # load and query this new json file
             json_new_query = json.loads(json_new)
-            dict = {}
+            print(type(json_new_query))
+
+            dict = {'name':[], 'likes':[]}
+
             for obj in json_new_query:
-                dict['name'] = obj['username']
-                dict['likes'] = obj['likes']
+                dict['likes'].append(obj['likes'])
+                dict['name'].append(obj['username'])
 
-
-
+            df = pandas.DataFrame(dict, columns=['name', 'likes'])
+            df1 = df.sort_index(by=['likes']) # most pandas methods return a new df, so has to assign it to a new df!!
+            print(df1)
 
     outfile.close()
+
 
 if __name__ == '__main__':
         main()
